e7c2933bc9e9cae57cd5e535820661e0b40d73e4
Alex <ivanchyshyn14@gmail.com>
2020-02-21 12:34:10 +0200

    add comments to env

--- env.sh
+++ env.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Database for Messages
 export DATABASE_URL = 'sqlite:///chat.db'
 
+# Database for Main Database (TopMarket, BuySell)
 export PROD_PG_USER = 'market_user'
 export PROD_PG_PASSWORD = 'pass12pass'
 export PROD_PG_HOST = 'localhost'
 export PROD_PG_PORT = '5432'
 export PROD_PG_NAME = 'market_dev'
 
+# Redis for caching
 export REDIS_HOST='localhost'
 export REDIS_PORT='6379'
